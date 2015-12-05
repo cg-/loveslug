607654f9c5433565f14b6536b6ee179268b5320b
Cole Grim <cgrim@ucsc.edu>
2015-12-04 23:51:38 -0800

    added tables for emails and chat messages

--- models/tables.py
+++ models/tables.py
@@ -25,3 +25,26 @@ db.define_table('person',
                 Field('password', 'password', requires=IS_NOT_EMPTY()),
                 Field('verify_password', 'password', requires=IS_NOT_EMPTY())
                 )
+
+# Chat Table
+# Will be used to hold chat messages for IMs.
+# - Cole
+db.define_table('chat',
+                Field('sender', requires=IS_NOT_EMPTY()),   # User who sent
+                Field('receiver', requires=IS_NOT_EMPTY()), # User who it's to'
+                Field('body', requires=IS_NOT_EMPTY()),     # Text in chat message
+                Field('seen', requires=IS_NOT_EMPTY()),     # Has it been seen?
+                Field('sent', requires=IS_NOT_EMPTY()),# Time it was sent
+                )
+
+# Message Table
+# Will be used to hold e-mails between two users.
+# - Cole
+db.define_table('email',
+                Field('sender', requires=IS_NOT_EMPTY()),   # User who sent
+                Field('receiver', requires=IS_NOT_EMPTY()), # User who it's to'
+                Field('body', requires=IS_NOT_EMPTY()),     # Text in chat message
+                Field('subject', requires=IS_NOT_EMPTY()),  # Subject of message
+                Field('seen', requires=IS_NOT_EMPTY()),     # Has it been seen?
+                Field('sent', requires=IS_NOT_EMPTY()),# Time it was sent
+                )
